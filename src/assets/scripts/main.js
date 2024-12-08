8287af4f46b0d483f679b2be276cd678f8898e88
Nicolau <nrocabus@uoc.edu>
2024-12-08 01:39:34 +0100

    Functional page

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -12,6 +12,20 @@ import * as bootstrap from 'bootstrap';
  * Write any other JavaScript below
  */
 
+
+switch(window.location.pathname){
+  case "/pagina1.html":
+    document.getElementsByClassName("nav-item")[0].classList.add("nav-item--active");
+    break;
+  case "/pagina2.html":
+    document.getElementsByClassName("nav-item")[1].classList.add("nav-item--active");
+    break;
+  case "/pagina3.html":
+    document.getElementsByClassName("nav-item")[2].classList.add("nav-item--active");
+    break;
+}
+
+
 +( function() {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
